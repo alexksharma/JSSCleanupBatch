ed5fc2424d72bf56ccc67e2b1bf9b8165cb0f52f
alexksharma <alexksharma@gmail.com>
2019-01-07 17:14:41 +0530

    Create CleanupApp.bat
    Put this batch file in root directory and run it to remove all of the default sample content (the Styleguide and GraphQL routes) from Sitecore JSS react app

---
+++ CleanupApp.bat
@@ -0,0 +1,17 @@
+REM Batch file to remove all of the default sample content (the Styleguide and GraphQL routes) 
+
+FOR /D %%p IN (".\src\components\Styleguide*") DO RMDIR "%%p" /s /q
+FOR /D %%p IN (".\src\components\GraphQL*") DO RMDIR "%%p" /s /q
+
+DEL /q ".\sitecore\definitions\components\Styleguide*"
+DEL /q ".\sitecore\definitions\components\GraphQL*"
+DEL /q ".\sitecore\definitions\templates\Styleguide*"
+
+FOR /D %%p IN (".\data\component-content\Styleguide") DO RMDIR "%%p" /s /q
+
+FOR /D %%p IN (".\data\content\Styleguide") DO RMDIR "%%p" /s /q
+
+FOR /D %%p IN (".\data\routes\styleguide") DO RMDIR "%%p" /s /q
+FOR /D %%p IN (".\data\routes\graphql") DO RMDIR "%%p" /s /q
+
+DEL /q ".\data\dictionary\*.yml"
